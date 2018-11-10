77e48cea3300950db6e4aa08c6e3a3e78d9a9254
efrance1 <efrance1@asu.edu>
2018-11-10 16:18:20 -0700

    cleaned up attributes for Red Tailed Hawk, created stubs for functions

--- js/redTailedHawk.js
+++ js/redTailedHawk.js
@@ -1,45 +1,67 @@
-function RedTailedHawk(color, size, weight, speed) {
+function RedTailedHawk(gender, color, size, speed) {
     this.color = color;
+    this.gender = gender;
     this.size = size;
-    this.weight = weight;
     this.speed = speed;
     this.scientificName = 'Buteo jamaicensis';
     this.commonName = 'Red-Tailed Hawk';
-    this.type = 'Bird'
+    this.animalType = 'Bird'
     this.diet = 'Carnivore';
     this.avgLifeSpan = '21 years';
 
-    //Reference: https://www.nationalgeographic.com/animals/birds/r/red-tailed-hawk/
-    if (this.size.toLowerCase() === 'small') {
-        this.body = (Math.random() * 18.0) + 20.7;
-        this.wingspan = (Math.random() * 38.0) + 39.7;
-    } else if (this.size.toLowerCase() === 'medium') {
-        this.body = (Math.random() * 20.8) + 23.4;
-        this.wingspan = (Math.random() * 39.8) + 41.4;
-    } else if (this.size.toLowerCase() === 'large') {
-        this.body = (Math.random() * 23.5) + 26.0;
-        this.wingspan = (Math.random() * 41.5) + 43.0;
-    }
-
-    if (this.weight.toLowerCase() === 'light') {
-        this.massStandard = (Math.random() * 24.3) + 33.4;
-        this.massMetric;
-    } else if (this.weight.toLowerCase() === 'normal') {
-        this.massStandard = (Math.random() * 33.5) + 42.5;
-        this.massMetric;
-    } else if (this.weight.toLowerCase() === 'heavy') {
-        this.massStandard = (Math.random() * 42.6) + 51.5;
-        this.massMetric;
+    //Reference: https://www.allaboutbirds.org/guide/Red-tailed_Hawk/id
+    if (this.gender.toLowerCase() === 'male') {
+        if (this.size.toLowerCase() === 'small') {
+            this.lengthInches = (Math.random() * 17.7) + 19.2;
+            this.weightOunces = (Math.random() * 24.3) * 31.5;
+            this.wingspanInches = (Math.random() * 44.9) + 47.4;
+        } else if (this.size.toLowerCase() === 'medium') {
+            this.lengthInches = (Math.random() * 19.3) + 20.8;
+            this.weightOunces = (Math.random() * 31.6) * 38.8;
+            this.wingspanInches = (Math.random() * 47.5) + 49.9;
+        } else if (this.size.toLowerCase() === 'large') {
+            this.lengthInches = (Math.random() * 20.9) + 22.1;
+            this.weightOunces = (Math.random() * 38.9) * 45.9;
+            this.wingspanInches = (Math.random() * 50.0) + 52.4;
+        }
+    } else if (this.gender.toLowerCase() === 'female') {
+        if (this.size.toLowerCase() === 'small') {
+            this.lengthInches = (Math.random() * 17.7) + 20.7;
+            this.weightOunces = (Math.random() * 31.8) * 38.4;
+            this.wingspanInches = (Math.random() * 44.9) + 47.4;
+        } else if (this.size.toLowerCase() === 'medium') {
+            this.lengthInches = (Math.random() * 20.8) + 23.4;
+            this.weightOunces = (Math.random() * 38.5) * 45.0;
+            this.wingspanInches = (Math.random() * 47.5) + 49.9;
+        } else if (this.size.toLowerCase() === 'large') {
+            this.lengthInches = (Math.random() * 23.5) + 26.0;
+            this.weightOunces = (Math.random() * 45.1) * 51.5;
+            this.wingspanInches = (Math.random() * 50.0) + 52.4;
+        }
     }
 
     if (this.speed.toLowerCase() === 'slow') {
-        this.speedMPH;
-        this.speedKPH;
+        this.speedMph = (Math.random() * 10) + 20;
     } else if (this.speed.toLowerCase() === 'fast') {
-        this.speedMPH;
-        this.speedKPH;
+        this.speedMph = (Math.random() * 51) + 121;
     } else {
-        this.speedMPH;
-        this.speedKPH;
+        this.speed = 'normal';
+        this.speedMph = (Math.random() * 21) + 50;
+    }
+
+    this.hover = function() {
+        //TODO: create logic to have Hawk hover in circular pattern.
+    }
+
+    this.dive = function() {
+        //TODO: create logic to have Hawk dive after prey.
+    }
+
+    this.reproduce = function() {
+        //TODO: create logic to have Hawk reproduce if object is paired with a mate.
+    }
+
+    this.feed = function() {
+        //TODO: create logic to have Hawk feed on captured prey.
     }
 }
