1747afe9f17a2c0f42416461bb28b1d9acbfcb26
Maxwell Heller <mhheller@asu.edu>
2018-11-22 17:33:43 +0100

    Adding initial integration of Three.js into a component

---
+++ src/components/ElementButton.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class ElementButton extends Component {
+    
+
+    render(){
+        return (
+            <li className="dropdown1">
+                <a href="javascript:void(1)" className="dropbtn2">{this.props.addText}</a>
+                <div className="dropdown-content1">
+                    <a onClick={(e) => this.props.increment(this.props.name)} href="#">{this.props.title}</a>
+                </div>
+            </li>
+        )
+    }
+
+}
+
+export default ElementButton
