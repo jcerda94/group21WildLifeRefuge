a65d8edcb9d8a203d085b407f9074c15ac18be6b
Maxwell Heller <mhheller@asu.edu>
2018-11-28 16:02:24 +0100

    Added orbit controls and dynamic resizing to the three.js visualization

--- src/components/SimViewer.js
+++ src/components/SimViewer.js
@@ -1,49 +1,79 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import {OrbitControls} from "../js/three/OrbitControls";
+
 class SimViewer extends Component {
     constructor(Super) {
         super();
 
+        this.state = {
+            height: 0,
+            width: 0
+        };
+
+
         var scene = new THREE.Scene();
         var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        this.camera = camera;
 
-        var renderer = new THREE.WebGLRenderer(document.body.clientWidth, document.body.clientHeight);
+        var renderer = new THREE.WebGLRenderer();
+        // TODO: Get renderer to render to the full component size
+        renderer.setSize(600, 600);
         this.renderer = renderer;
-        document.body.appendChild( renderer.domElement );
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        var cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+        // create the ground plane
+        var planeGeometry = new THREE.PlaneGeometry(20, 5, 10);
+        var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
 
-        var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-        var geometry = new THREE.Geometry();
-        geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
-        geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
-        geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+        scene.add(plane);
+        // add the plane to the scene
+//        scene.add(plane);
+        // position and point the camera to the center of the scene
+        camera.position.x = 3;
+        camera.position.y = 15;
+        camera.position.z = 10;
+        camera.lookAt(scene.position);
 
-        var line = new THREE.Line( geometry, material );
-        scene.add( line );
-
-
-        camera.position.z = 20;
+        // add spotlight for the shadows
+        var spotLight = new THREE.DirectionalLight(0xffffff);
+        spotLight.position.set(50, 50, 50);
+        spotLight.castShadow = true;
+        spotLight.intensity = 2;
+        scene.add(spotLight);
+        var ambiLight = new THREE.AmbientLight(0x333333);
+        scene.add(ambiLight);
 
         var animate = function () {
             requestAnimationFrame( animate );
 
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
 
-            renderer.render( scene, camera );
+            renderer.render(scene, camera );
         };
 
         animate();
 
+    }
+
+    componentDidUpdate() {
+
+        console.log(this.props.height);
+
+
+        var cameraControl = new OrbitControls(this.camera);
+        cameraControl.maxPolarAngle = Math.PI * 0.5;
+
+        this.divElement.appendChild( this.renderer.domElement );
+        this.renderer.setSize(window.innerWidth, this.props.height + 50);
+
 
     }
+
+
     render(){
         return (
-            <div style={this.props.style}>
+            <div style={this.props.style} ref={ divElement => {this.divElement = divElement}}>
 
             </div>
         )
