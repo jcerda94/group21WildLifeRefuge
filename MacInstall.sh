d142ec57208c8e85fb8c1892e58c9d752c1added
Maxwell Heller <mhheller@asu.edu>
2018-11-30 17:33:38 +0100

    Added simple Mac Installation script to simplify intial use

---
+++ MacInstall.sh
@@ -0,0 +1,7 @@
+# Initial Install Script for Wildlife Refuge Project
+npm install
+
+echo All Modules Installed, the project will now launch.
+echo In the future just type 'npm run' to launch the project
+sleep 10
+npm start
