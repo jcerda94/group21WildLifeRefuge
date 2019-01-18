3a3d53085a1b49f4dd24f142c2c127164b5bba3c
tduangbo <tduangbo@asu.edu>
2019-01-18 02:12:13 -0800

    Making changes on Camera UI control

--- src/components/CamraControlUI.js
+++ src/components/CamraControlUI.js
@@ -8,7 +8,7 @@ import DatGui, { DatBoolean, DatButton, DatNumber, DatString } from 'react-dat-g
 class CamraControlUI extends Component {
     state = {
         data: {
-            package: 'react-dat-gui',
+            package: 'Choose a Camera',
             power: 9000,
             isAwesome: true,
             feelsLike: '#2FA1D6',
@@ -25,15 +25,7 @@ class CamraControlUI extends Component {
                 <DatString path='package' label='Camera Control'/>
 
                 <DatBoolean path='bloolena' label='Camera ONE'/>
-                <DatNumber path='power' label='Power' min={9000} max={9999} step={1} />
-                <DatBoolean path='boolean' label='Camera TWO'/>
-                <DatNumber path='power' label='Power' min={9000} max={9999} step={1} />
-            
-                <DatNumber path='power' label='Power' min={9000} max={9999} step={1} />
 
-                <DatBoolean path='isAwesome' label='Camera THREE'/>
-                <DatNumber path='power' label='Power' min={9000} max={9999} step={1} />
-                <DatBoolean path='isAwesome' label='Camera FOUR'/>
 
             </DatGui>
         )
