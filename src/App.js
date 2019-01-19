34cc7c2de3773872b96ce2acb0e03263161f667d
Andrew Thyng <andrew.thyng@gmail.com>
2019-01-19 07:14:24 -0600

    Fixed onclick button behavior so the sim is actually loaded

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ import AuthorView from "./components/AuthorView";
 class App extends Component {
   constructor (props) {
     super(props);
-    this.state = JSON.parse(JSON.stringify(model));
+    this.state = {
+      toggleContext: false,
+      simContext: "VIEWER",
+      studentMode: true,
+      authorMode: false,
+      reviewMode: false,
+      simEnabled: true,
+      redCedar: 0,
+      redHawk: 0,
+      snowHare: 0,
+      bigSage: 0,
+      level: 0,
+      simObjects: [],
+      loggedIn: false
+    };
 
     this.simModel = new window.simcapi.CapiAdapter.CapiModel(this.state);
 
@@ -98,11 +112,19 @@ class App extends Component {
     this.addSimulationElement(objectName);
   }
 
+  handleLogin = () => {
+    if (!this.state.loggedIn) {
+      this.setState({ loggedIn: true }, () => console.log(this.state));
+    }
+  }
+
   render () {
     // TODO: Add state checking using -> simcapi.Transporter.getConfig().context
     var display = null;
     if (!this.state.loggedIn) {
-      display = <WelcomePage name={this.simModel} />;
+      display = (
+        <WelcomePage name={this.simModel} handleLogin={this.handleLogin} />
+      );
     } else if (this.state.studentMode) {
       display = <StudentView increment={this.increment} />;
     } else if (this.state.authorMode) {
