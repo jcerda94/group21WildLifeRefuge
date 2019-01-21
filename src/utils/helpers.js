98f685b810750a65ecdad4355f1eef286358a9a7
Andrew Thyng <andrew.thyng@gmail.com>
2019-01-21 09:14:10 -0600

    Added getValue function helpers
    The function solves a pretty irritating problem with javascript,
    which is trying to access keys on null or undefined objects.
    Instead of pulling in a large library like Lodash I wrote this
    simpler function. It uses dot notation to get nested values from
    an object, such as getValue('name.first', personObject).

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,23 @@
 export const random = (min, max) => {
   return Math.random() * (max - min) + min;
 };
+
+export const getValue = (accessor = "", initial = {}) => {
+  if (initial === null) {
+    throw new Error(`Cannot access ${accessor} on null initial value`);
+  }
+  if (accessor === null) {
+    throw new Error("Cannot access null property of an object");
+  }
+
+  return accessor.split(".").reduce((accum, accessor, i, arr) => {
+    const val = accum[accessor];
+    const valueIsLast = arr.length - 1 === i;
+
+    if (valueIsLast) {
+      return val;
+    }
+
+    return val || {};
+  }, initial);
+};
