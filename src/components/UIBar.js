2d828c6fac9f4d7efc7c910f2bd2aec58df817c5
jcerda94 <jwcerda@asu.edu>
2019-01-22 20:36:57 -0800

    Update UIBar.js

--- src/components/UIBar.js
+++ src/components/UIBar.js
@@ -1,31 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ElementButton from "./ElementButton";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  height: 60px;
+  background-color: #303030;
+`;
 
 class UIBar extends Component {
-
-
-    componentDidMount() {
-        this.props.reportHeight(this.divElement.clientHeight)
-    }
-
-    render(){
-
-        return (
-            <ul ref={divElement => {this.divElement = divElement}}>
-                <ElementButton model="tree" key="rC" addText="Add Tree" title="Western Red Cedar" increment={this.props.increment} name="redCedar"/>
-                <ElementButton model="remove tree" key="rmC" addText="Remove Tree" title="Western Red Cedar" increment={this.props.increment} name="redCedar"/>
-                <ElementButton model="hawk" key="rH" addText="Add Predator" title="Red Tailed Hawk" increment={this.props.increment} name="redHawk"/>
-                <ElementButton model="remove hawk" key="rmH" addText="Remove Hawk" title="Red Tailed Hawk" increment={this.props.increment} name="redCedar"/>
-                <ElementButton model="hare" key="sH" addText="Add Prey" title="Snowshoe Hare" increment={this.props.increment} name="snowHare"/>
-                <ElementButton model="Remove hare" key="rmH" addText="Remove Prey" title="Snowshoe Hare" increment={this.props.increment} name="snowHare"/>
-                <ElementButton model="bush" key="bS" addText="Add Bushes" title="Big Sagebrush" increment={this.props.increment} name="bigSage"/>
-                <ElementButton model="Remove bush" key="rmBS" addText="Remove Bushes" title="Big Sagebrush" increment={this.props.increment} name="bigSage"/>
-                <ElementButton model="wheatgrass" key = "bW" addText= "Add Wheatgrass" title="BlueBunch Wheatgrass" increment={this.props.increment} name="blueBunch"/>
-                <ElementButton model="Remove wheatgrass" key = "rmBW" addText= "Remove Wheatgrass" title="BlueBunch Wheatgrass" increment={this.props.increment} name="blueBunch"/>
-            </ul>
-        )
-    }
-
+  render () {
+    return (
+      <Container>
+        <ElementButton
+          model='tree'
+          key='rC'
+          addText='Add Tree'
+          title='Western Red Cedar'
+          increment={this.props.increment}
+          name='redCedar'
+        />
+         <ElementButton model="remove tree" key="rmC" addText="Remove Tree" title="Western Red Cedar" 
+                increment={this.props.increment} name="redCedar"/>;
+        <ElementButton
+          model='hawk'
+          key='rH'
+          addText='Add Predator'
+          title='Red Tailed Hawk'
+          increment={this.props.increment}
+          name='redHawk'
+        />
+        <ElementButton model="remove hawk" key="rmH" addText="Remove Hawk" title="Red Tailed Hawk" 
+                increment={this.props.increment} name="redCedar"/>;
+                
+        <ElementButton
+          model='hare'
+          key='sH'
+          addText='Add Prey'
+          title='Snowshoe Hare'
+          increment={this.props.increment}
+          name='snowHare'
+        />
+        <ElementButton model="Remove hare" key="rmH" addText="Remove Prey" title="Snowshoe Hare" 
+                increment={this.props.increment} name="snowHare"/>;
+                
+        <ElementButton
+          model='bush'
+          key='bS'
+          addText='Add Bushes'
+          title='Big Sagebrush'
+          increment={this.props.increment}
+          name='bigSage'
+        />
+        <ElementButton model="Remove bush" key="rmBS" addText="Remove Bushes" title="Big Sagebrush" 
+                increment={this.props.increment} name="bigSage"/>;
+        <ElementButton model="wheatgrass" key = "bW" addText= "Add Wheatgrass" title="BlueBunch Wheatgrass" 
+                increment={this.props.increment} name="blueBunch"/>;
+        <ElementButton model="Remove wheatgrass" key = "rmBW" addText= "Remove Wheatgrass" 
+                title="BlueBunch Wheatgrass" increment={this.props.increment} name="blueBunch"/>;
+            
+      </Container>
+    );
+  }
 }
 
-export default UIBar
+export default UIBar;
