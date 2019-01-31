eb72c82d07575f7ad916eacef2ff7a94f4ddd612
jcerda94 <jwcerda@asu.edu>
2019-01-31 12:54:06 -0800

    Remove menu icon

--- src/components/UI/UIBar.js
+++ src/components/UI/UIBar.js
@@ -8,7 +8,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/core/Menu";
+
 
 const styles = {
   root: {
@@ -29,7 +29,6 @@ function UIBar(props) {
       <AppBar position="static">
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" className={classes.grow}>
             Willapa Refuge
