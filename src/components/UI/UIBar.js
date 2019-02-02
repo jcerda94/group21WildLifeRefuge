9c3b5dd712aeb9af2ebbad442302b96d6b759876
Andrew Thyng <andrew.thyng@gmail.com>
2019-02-02 09:24:40 -0600

    Removed unused icon button container

--- src/components/UI/UIBar.js
+++ src/components/UI/UIBar.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
 
 const styles = {
   root: {
@@ -19,37 +18,34 @@ const styles = {
     marginLeft: -12,
     marginRight: 20
   }
-};
-function UIBar(props) {
-  const { classes } = props;
+}
+function UIBar (props) {
+  const { classes } = props
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position='static'>
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
+          <Typography variant='h6' color='inherit' className={classes.grow}>
             Willapa Refuge
           </Typography>
-          <Button color="inherit">Add Predator</Button>
-          <Button color="inherit">Remove Predator</Button>
-          <Button color="inherit">Add Prey</Button>
-          <Button color="inherit">Remove Prey</Button>
-          <Button color="inherit">Add Trees</Button>
-          <Button color="inherit">Remove Trees</Button>
-          <Button color="inherit">Add Bushes</Button>
-          <Button color="inherit">Remove Bushes</Button>
-          <Button color="inherit">Add Grasses</Button>
-          <Button color="inherit">Remove Grasses</Button>
+          <Button color='inherit'>Add Predator</Button>
+          <Button color='inherit'>Remove Predator</Button>
+          <Button color='inherit'>Add Prey</Button>
+          <Button color='inherit'>Remove Prey</Button>
+          <Button color='inherit'>Add Trees</Button>
+          <Button color='inherit'>Remove Trees</Button>
+          <Button color='inherit'>Add Bushes</Button>
+          <Button color='inherit'>Remove Bushes</Button>
+          <Button color='inherit'>Add Grasses</Button>
+          <Button color='inherit'>Remove Grasses</Button>
         </Toolbar>
       </AppBar>
     </div>
-  );
+  )
 }
 
 UIBar.propTypes = {
   classes: PropTypes.object.isRequired
-};
+}
 
-export default withStyles(styles)(UIBar);
-
+export default withStyles(styles)(UIBar)
