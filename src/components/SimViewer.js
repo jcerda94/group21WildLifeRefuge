ec1f687bf2f4b815f95e00f02b123b896b28e73e
jcerda94 <jwcerda@asu.edu>
2019-02-04 17:26:41 -0800

    Moved the buttons to the bottom center. now trying to separate.

--- src/components/SimViewer.js
+++ src/components/SimViewer.js
@@ -7,9 +7,7 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
-import Icon from "@material-ui/core/Icon";
 import DeleteIcon from "@material-ui/icons/Delete";
-import NavigationIcon from "@material-ui/icons/Navigation";
 
 const CanvasContainer = styled.div`
   flex: 1;
@@ -18,33 +16,14 @@ const CanvasContainer = styled.div`
 
 const styles = theme => ({
   fab: {
-    margin: 0,
-    top: "auto",
-    right: 20,
-    bottom: 20,
-    left: "auto",
-    position: "fixed",
+    margin: theme.spacing.unit,
   },
 });
 
-const Button = styled.div`
-  padding: 14px;
-  color: white;
-  font-size: 18px;
-  text-align: center;
-  border-radius: 20px;
-  background-color: tomato; 
-  position: absolute;
-  top: 85px;
-  left: 25px;
-  cursor: pointer;
-  user-select: none;
-`;
-
 const Container = styled.div`
  position: absolute;
- left: 15%;
- top: 40%;
+ left: 45%;
+ top: 90%;
  display: flex;
  flex-direction: column;
  align-items: flex-start;
