f6da316ef15cc7cd18ee68872a1e0690a5fae7ef
Maxwell Heller <mhheller@asu.edu>
2019-02-21 16:20:14 +0700

    Added code to properly initialize an array with environment condition objects, added polyfills for IE support

--- src/scenes/EnvironmentManager.js
+++ src/scenes/EnvironmentManager.js
@@ -1,9 +1,103 @@
+import {getSceneManager} from "./SceneManager";
+
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill#Polyfill
+// Used to support Internet Explorer
+if (!Array.prototype.fill) {
+    Object.defineProperty(Array.prototype, 'fill', {
+        value: function(value) {
+
+            // Steps 1-2.
+            if (this == null) {
+                throw new TypeError('this is null or not defined');
+            }
+
+            var O = Object(this);
+
+            // Steps 3-5.
+            var len = O.length >>> 0;
+
+            // Steps 6-7.
+            var start = arguments[1];
+            var relativeStart = start >> 0;
+
+            // Step 8.
+            var k = relativeStart < 0 ?
+                Math.max(len + relativeStart, 0) :
+                Math.min(relativeStart, len);
+
+            // Steps 9-10.
+            var end = arguments[2];
+            var relativeEnd = end === undefined ?
+                len : end >> 0;
+
+            // Step 11.
+            var final = relativeEnd < 0 ?
+                Math.max(len + relativeEnd, 0) :
+                Math.min(relativeEnd, len);
+
+            // Step 12.
+            while (k < final) {
+                O[k] = value;
+                k++;
+            }
+
+            // Step 13.
+            return O;
+        }
+    });
+}
+
+// From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+// Used to support Internet Explorer and Android Webviews
+if (typeof Object.assign != 'function') {
+    // Must be writable: true, enumerable: false, configurable: true
+    Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) { // .length of function is 2
+            'use strict';
+            if (target == null) { // TypeError if undefined or null
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var to = Object(target);
+
+            for (var index = 1; index < arguments.length; index++) {
+                var nextSource = arguments[index];
+
+                if (nextSource != null) { // Skip over if undefined or null
+                    for (var nextKey in nextSource) {
+                        // Avoid bugs when hasOwnProperty is shadowed
+                        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                            to[nextKey] = nextSource[nextKey];
+                        }
+                    }
+                }
+            }
+            return to;
+        },
+        writable: true,
+        configurable: true
+    });
+}
+
+// TODO: Maybe use this to generate the ground plane? Or have the ground plane model be generated and then sampled?
 class EnvironmentManager {
 
-    localEnv = [100][100];
+
+    localEnv = null;
+    defaultEnvironmentObject = {
+      water: 1.0
+    };
 
     constructor(){
+        const sceneManger = getSceneManager();
 
+        //Added 1 to array size to handle odd ground sizes (1222 x 899) and objects at the absolute edge of the ground
+        const groundX = Math.trunc(sceneManger.groundSize['x']/10) + 1;
+        const groundY = Math.trunc(sceneManger.groundSize['y']/10) + 1;
+
+        //Initializes an array shaped like our ground object, and fills it with a set of default environment conditions
+        //TODO: Add dynamically generated environments (non-uniform starting conditions, maybe toy environment 'painter')
+        this.localEnv = [...Array(groundX)].map(x=>Array(groundY).fill(Object.assign({}, this.defaultEnvironmentObject)));
     }
 
     getEnvByXYPos(x, y){
