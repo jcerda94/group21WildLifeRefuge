d12afa32bcc4f68649c441823c839ea7727c9e52
tduangbo <tduangbo@asu.edu>
2019-03-20 16:52:35 -0700

    add conditions such as there must be two tree for new a new tree to germinate.

--- src/scenes/AddModelsBasedOnSimTime.js
+++ src/scenes/AddModelsBasedOnSimTime.js
@@ -6,22 +6,28 @@
 
 import {getSimTime} from "../components/UI/DisplayTime";
 import AddModels from "./AddModels";
+import {getSceneManager} from "./SceneManager";
 const maxAmountOfTree = 15;
 let timeInterval = 1;
 let days = timeInterval;
+
 function AddModelsBasedOnSimTime() {
 
-
+const sceneManager = getSceneManager();
     const simTime = getSimTime();
+    let numberOfTrees = 0;
+    const trees = sceneManager.getSceneObjectsOf({ types: ["Tree"] });
+    numberOfTrees = trees.length;
+
    //adding tree every 1 days (simulation time)
     if( parseInt(simTime.getDay()) >= days && parseInt(simTime.getDay()%(timeInterval)) == 0){
-        addTree();
+        if(numberOfTrees < maxAmountOfTree && numberOfTrees >= 2){
+            new AddModels("tree");
+        }
         days = days +timeInterval;
     }
 
-    function addTree(){
-       new AddModels("tree");
-    }
+
     function addGras() {
         //TODO: need logic for adding grass
 
